ed1bad523e4a37648882e9bf81b8ff65453d7a48
ericgaspar <junk.eg@free.fr>
2020-10-23 00:40:31 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-gd"
-
 YNH_PHP_VERSION="7.4"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-gd"
 
 #=================================================
 # PERSONAL HELPERS
