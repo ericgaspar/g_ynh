3f0dab9469422f826de55c83a64b0cd9e146ec4e
ericgaspar <junk.eg@free.fr>
2020-11-29 15:14:52 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="7.3"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-gd"
 
